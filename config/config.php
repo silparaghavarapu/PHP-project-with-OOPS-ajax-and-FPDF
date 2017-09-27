c509a00697f7b96193c9a68e94cc3ab3a6600775
silparaghavarapu <silpa.raghavarapu@gmail.com>
2017-09-27 13:30:05 -0400

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+define('DEVELOPMENT',true);
+
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'specialitytradeunion');
+define('DB_USER', 'root');
+define('DB_PASS', 'password');
+
+
+define('BASEPATH', 'http://localhost/SpecialityTradeUnion/');
+define('MODELPATH','Models/');
+define('CONTROLLERPATH','Controllers/');
+define('VIEWPATH','Views/');
+define('LIBPATH','libs/');
+define('SHOPUPLOAD','public/Shop_Documents/');
+define('MEMBERSUPLOAD','public/Members_Documents/');
+define('MEMBERSPHOTOS','public/Members_photos/');
+define('DEFAULTCONTROLLER', 'main');
+DEFINE('SESSION_SALT',  'SALT_FOR_SESSION');
+
