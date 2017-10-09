fdc2ac9de2fa357984144e982eb7d602f3347e2f
silparaghavarapu <silpa.raghavarapu@gmail.com>
2017-10-09 17:11:36 -0400

    Add files via upload

--- Models/Model_Insurance.php
+++ Models/Model_Insurance.php
@@ -4,15 +4,14 @@ class Model_Insurance extends Dbh{
     
     public function setInsurance()
     {
-        // echo "inside function<br>";
+        
         $sql="insert into insurance (Insurance_Name,insurance_plan,Modified_By,Modified_date) values ('".$_POST['Insurance_name']."','".$_POST['Insurance_plan']."','".$_SESSION['empid']."','".Date("Y/m/d")."')";
-        // $results=$this->connect()->query($sql);
-        //echo $this->connect()->query($sql)."<br>";
+        
         if ($this->connect()->query($sql) === TRUE) {
             
             $_POST['path']="mainpage";
         } else {
-            echo "Error: " . $sql . "<br>" . $this->connect()->error;
+            echo "Error: Record Already Exits" . $this->connect()->error;
         }
     }
     protected function getInsurance()
@@ -31,6 +30,47 @@ class Model_Insurance extends Dbh{
         else
         {
             echo "Error: " . $sql . "<br>" . $this->connect()->error;
+            $_SESSION['error']=$this->connect()->error;
+        }
+        
+    }
+   
+    protected function getHistoryInsurance($f,$l)
+    {
+        $getsql="select b.rowcount,history_insurance.*  from history_insurance  INNER JOIN ( SELECT COUNT(*) as rowcount FROM history_insurance) b LIMIT ".$f.",".$l;
+        $result=$this->connect()->query($getsql);
+        $data="";
+        if($result<> null)
+        {
+            while($row=$result->fetch_assoc()){
+                $data[]=$row;
+                
+            }
+            return $data;
+        }
+        else
+        {
+            echo "Error: " . $getsql . "<br>" . $this->connect()->error;
+            $_SESSION['error']=$this->connect()->error;
+        }
+        
+    }
+    protected function getInsurancehistoryid($id,$f,$l)
+    {
+        $getsql="select b.rowcount,history_insurance.* from history_insurance INNER JOIN ( SELECT COUNT(*) as rowcount FROM history_insurance) b where Insurance_ID=".$id." LIMIT ".$f.",".$l;
+        $result=$this->connect()->query($getsql);
+        $data="";
+        if($result<> null)
+        {
+            while($row=$result->fetch_assoc()){
+                $data[]=$row;
+                
+            }
+            return $data;
+        }
+        else
+        {
+            echo "Error: " . $getsql . "<br>" . $this->connect()->error;
             $_SESSION['error']=$this->connect()->error;
         }
         
@@ -55,19 +95,14 @@ class Model_Insurance extends Dbh{
         }
         
     }
-    protected function DelRecordInsurance()
+    protected function DelRecordInsurance($id)
     {
-        $getsql="insert into history_insurance (Insurance_ID,Insurance_Name,insurance_plan,Modified_By,Modified_date,Update_type) select Insurance_ID,Insurance_Name,insurance_plan,Modified_By,Modified_date,'Delete' as update_type from insurance where Insurance_ID=".$_SESSION['insuranceid'];
-        $results=$this->connect()->query($getsql);
-        $delsql="DELETE from insurance where Insurance_ID=".$_SESSION['insuranceid'];
+       
+        $delsql="DELETE from history_insurance where id=".$id;
         $result=$this->connect()->query($delsql);
         $data="";
         if($result<> null)
         {
-            while($row=$result->fetch_assoc()){
-                $data[]=$row;
-                
-            }
             return $data;
         }
         else
@@ -77,7 +112,7 @@ class Model_Insurance extends Dbh{
         }
         
     }
-    protected function getUpdateInsurance()
+    public function getUpdateInsurance()
     {
         $data="";
         if($_POST['Insurance_name']<>$_POST['Insurance_name1'] || $_POST['Insurance_plan']<>$_POST['Insurance_plan1'])
@@ -85,15 +120,12 @@ class Model_Insurance extends Dbh{
         
         $getsql="insert into history_insurance (Insurance_ID,Insurance_Name,insurance_plan,Modified_By,Modified_date,Update_type) select Insurance_ID,Insurance_Name,insurance_plan,Modified_By,Modified_date,'Modify' as update_type from insurance where Insurance_ID=".$_SESSION['insuranceid'];
         $results=$this->connect()->query($getsql);
-        $updatesql="update insurance set Insurance_Name='".$_POST['Insurance_name']."',insurance_plan='".$_POST['Insurance_name']."',Modified_By='".$_SESSION['empid']."',Modified_date='".Date("Y/m/d")."' where Insurance_ID=".$_SESSION['insuranceid'];
+        $updatesql="update insurance set Insurance_Name='".$_POST['Insurance_name']."',insurance_plan='".$_POST['Insurance_plan']."',Modified_By='".$_SESSION['empid']."',Modified_date='".Date("Y/m/d")."' where Insurance_ID=".$_SESSION['insuranceid'];
         $result=$this->connect()->query($updatesql);
         if($result<> null)
         {
-            while($row=$result->fetch_assoc()){
-                $data[]=$row;
-                
-            }
-            return $data;
+            $_POST['path']="mainpage";
+           return $data;
         }
         else
         {
